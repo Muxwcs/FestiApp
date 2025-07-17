079c76d8f835aa3f1a06c744b962c8cf0cf00283
Mux RBX <g.riberoux@gmail.com>
2025-07-17 13:03:15 +0200

    base architecture and components for caalendar dashboard display

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,8 +1,11 @@
+import { CalendarProvider } from "@/components/calendar/context/calendar-context"
 import AppSidebar from "@/components/layout/app-sidebar"
 import Topbar from "@/components/layout/topbar"
+import { getEvents, getUsers } from "@/components/calendar/requests"
 
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
   const userRole: "admin" | "bénévole" = "admin" // Replace with dynamic logic
+  const [events, users] = await Promise.all([getEvents(), getUsers()])
 
   return (
     <div className="flex min-h-screen w-full">
@@ -10,7 +13,10 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
       <div className="flex flex-col flex-1">
         <Topbar />
         <main className="flex-1 p-4">
-          {children}
+          <CalendarProvider users={users} events={events}>
+
+            {children}
+          </CalendarProvider>
         </main>
       </div>
     </div>
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,22 +1,32 @@
 "use client"
 
+import { ClientContainer } from "@/components/calendar/client-container"
+import { useCalendar } from "@/components/calendar/context/calendar-context"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { useLoadUserProfile } from "@/hooks/useLoadUserProfile"
 import { useUserStore } from "@/stores/userStore"
-import { Suspense } from "react"
 
 const Dashboard = () => {
   useLoadUserProfile()
   const user = useUserStore((state) => state.user)
-
+  const { view } = useCalendar()
   return (
-    <>
-      <div>Dashboard</div>
-      <Suspense fallback={<div>Loading user data...</div>}>
-        <h1>Bienvenue, {user?.fields?.name}</h1>
-        {/* <h2>{data?.user.role}</h2> */}
-        <p>Rôle: {user?.fields?.role}</p>
-      </Suspense>
-    </>
+    <div className="min-h-screen p-4 sm:p-6 lg:p-8 space-y-6">
+      <Card className="p-4">
+        <CardHeader className="flex items-center gap-2">
+          <CardTitle className="flex items-center gap-2">
+            Dashboard
+          </CardTitle>
+          <p>Nom: {user?.fields?.name}</p>
+          <p>Email: {user?.fields?.email}</p>
+          <p>Rôle: {user?.fields?.role}</p>
+        </CardHeader>
+        <CardContent className="flex flex-col items-center gap-4 min-w-full">
+          <ClientContainer view={view} />
+        </CardContent>
+
+      </Card>
+    </div>
   )
 }
 
