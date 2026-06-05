a8531ba5fa7a3d94ece7bc849cc0350d3baabee7
Mux RBX <g.riberoux@gmail.com>
2026-06-05 19:07:50 +0200

    deleted files for PWA rules

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,35 +1,4 @@
-// import NextAuth from "next-auth"
-
-// declare module "next-auth" {
-//   interface Session {
-//     user: {
-//       id?: string | null
-//       name?: string | null
-//       email?: string | null
-//       role: string
-//       airtableId?: string | null,
-//       isReferent: boolean
-//     }
-//   }
-
-//   interface User {
-//     id: string
-//     airtableId: string
-//     name?: string | null
-//     email?: string | null
-//     role: string
-//     isReferent: boolean // ✅ Add to user type
-//   }
-// }
-
-// declare module "next-auth/jwt" {
-//   interface JWT {
-//     role: string
-//     isReferent: boolean // ✅ Add to JWT type
-//   }
-// }
-
-import type { Role } from "@/generated_old/prisma/client"
+import type { Role } from "@/generated/prisma/enums"
 import { type DefaultSession } from "next-auth"
 
 declare module "next-auth" {
